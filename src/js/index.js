0f74e6b856303819990d8fd02610bc5e7a4b1fdc
Duronne <duronne.tks@gmail.com>
2022-10-31 12:27:58 -0300

    first commmit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import {Router} from './router.js'
+
+const router = new Router()
+  router.add("/", "../src/pages/home.html")
+  router.add("/about", "../src/pages/about.html")
+  router.add("/contact", "../src/pages/contact.html")
+  router.add(404, "../src/pages/404.html")
+
+router.handle()
+window.onpopstate = () => { router.handle() }
+window.route = () => { router.route() }
